32aae67c7c7d669710c7dc7b16042397fe623854
samarth sikotara <samarthsikotara@gmail.com>
2016-05-09 15:39:40 +0530

    changes

---
+++ app/controllers/welcomes_controller.rb
@@ -0,0 +1,15 @@
+class WelcomesController < ApplicationController
+
+	def real_time
+    if request.method == "GET"
+      if params['hub.mode'] =='subscribe' && params['hub.verify_token'] =='stringToken'
+        render :text => params['hub.challenge']
+      else
+        render :text => 'Failed to authorize facebook challenge request'
+      end
+    elsif request.method == "POST"
+      #do stuff with information 
+    end
+  end
+
+end
