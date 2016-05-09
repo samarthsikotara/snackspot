ea34339f1dbeb531a490612da2b7c3aa953f90c8
samarth sikotara <samarthsikotara@gmail.com>
2016-05-09 15:23:10 +0530

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
-
+  
+  namespace :welcome do
+    match "/real-time", to: "welcome#real_time", :via => [:get,:post]
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
