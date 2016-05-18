8e24d0e001beec65fe123e2c90ee6b686f6011a8
samarth sikotara <samarthsikotara@gmail.com>
2016-05-18 20:50:12 +0530

    change

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -5,4 +5,4 @@ Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
 ENV["REDISTOGO_URL"] ||= "redis://redistogo:bc3e063d5b9bfca5e09aec2d884b1e4f@lab.redistogo.com:9543/"
 uri = URI.parse(ENV["REDISTOGO_URL"])
 
-Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
