89ccdf330038b511645da9233594e972be4603b7
samarth sikotara <samarthsikotara@gmail.com>
2016-07-15 13:42:41 +0530

    changes

--- app/controllers/api/v0/tweet_controller.rb
+++ app/controllers/api/v0/tweet_controller.rb
@@ -27,6 +27,7 @@ module Api::V0
 						:name => tweet["user"]["name"],
 						:description => tweet["user"]["description"], 
 						:screen_name => tweet["user"]["screen_name"],
+						:gender => (get_gender(tweet["user"]["name"])["gender"] rescue ""),
 						:profile_url => 'https://twitter.com/'+tweet["user"]["screen_name"].to_s, 
 						:latitude => tweet["geo"]["coordinates"][0], 
 						:longitude => tweet["geo"]["coordinates"][1], 
@@ -42,34 +43,6 @@ module Api::V0
 
 			render json: {:data => {:tweets => tweets.compact.sort_by { |k| k["created_at"]}, :people => people.compact.sort_by { |k| k["created_at"]}}}, status: 200	
 		end
-
-		def people
-			return render json: {:message => "Unable to get current location. please check your internet"}, status: 400 unless params[:lat] && params[:lng]
-			geo_data = tweet_data(params[:lat], params[:lng])
-			data = geo_data["statuses"].map do |tweet|
-				unless tweet["geo"].nil?
-					{
-						:id => tweet["user"]["id"],
-						:name => tweet["user"]["name"],
-						:description => tweet["user"]["description"], 
-						:screen_name => tweet["user"]["screen_name"],
-						:gender => get_gender(tweet["user"]["name"])["gender"],
-						:profile_url => 'https://twitter.com/'+tweet["user"]["screen_name"].to_s, 
-						:latitude => tweet["geo"]["coordinates"][0], 
-						:longitude => tweet["geo"]["coordinates"][1], 
-						:profile_pic_url => tweet["user"]["profile_image_url"], 
-						:profile_background_image_url => tweet["user"]["profile_background_image_url"], 
-						:followers_count => tweet["user"]["followers_count"],
-						:tweets_count => tweet["user"]["statuses_count"],
-						:following_count => tweet["user"]["friends_count"],
-						:created_at => Time.zone.parse(tweet["created_at"].to_s).to_i,
-					}
-				end
-			end
-			render json: {:data => data.compact.sort_by { |k| k["created_at"]}}, status: 200	
-		end
-
-		
 
 	end
 end
