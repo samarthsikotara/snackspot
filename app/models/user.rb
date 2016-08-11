7f298ca6919f2adf7cd182a82adbc1bfaaca8eef
samarth sikotara <samarthsikotara@gmail.com>
2016-08-12 00:38:53 +0530

    error fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,5 @@
 class User < ActiveRecord::Base
-	def self.from_omniauth(auth)
-    debugger
-    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.name = auth.info.name
-      user.oauth_token = auth.credentials.token
-      user.oauth_secret = auth.credentials.secret
-      user.save!
-    end
-  end
-
+	
   def tweet(tweet = "", tweet_id=nil)
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = Rails.application.config.twitter_key
